7927272ea86c8499bfde69117fee022a3d34a052
Bob Lacatena <Bob@Lacatena.net>
2013-01-17 12:33:56 -0500

    Add index page, ignore personal Debug settings

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	header('Content-type: text/html; charset=utf-8');
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+	<body>
+		<p>Welcome to the site.</p>
+	</body>
+</html>
